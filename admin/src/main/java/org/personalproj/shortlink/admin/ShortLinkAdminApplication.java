778107b505dc004e48192d48e38f60b56cf9d0eb
Zifeng-PAN <panzifeng00@163.com>
2024-01-26 22:07:32 +0800

    perfect project structure

---
+++ admin/src/main/java/org/personalproj/shortlink/admin/ShortLinkAdminApplication.java
@@ -0,0 +1,17 @@
+package org.personalproj.shortlink.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author panzifeng
+ * @date 2024/1/26 22:03
+ * @description ShortLinkAdminApplication
+ */
+
+@SpringBootApplication
+public class ShortLinkAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShortLinkAdminApplication.class,args);
+    }
+}
