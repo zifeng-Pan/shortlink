e662b69d807430d4e7aaa060e2680ddd390f00f7
Zifeng-PAN <panzifeng00@163.com>
2024-03-07 09:45:34 +0800

    feature:优化短链接分组响应实体字段

--- admin/src/main/java/org/personalproj/shortlink/admin/dto/resp/GroupRespDTO.java
+++ admin/src/main/java/org/personalproj/shortlink/admin/dto/resp/GroupRespDTO.java
@@ -29,12 +29,13 @@ public class GroupRespDTO {
     private String name;
 
     /**
-     * 创建分组用户名
-     */
-    private String username;
-
-    /**
      * 分组排序
      */
     private Integer sortOrder;
+
+    /**
+     *
+     * 当前分组下短链接数量统计
+     */
+    private Integer shortLinkCount;
 }
